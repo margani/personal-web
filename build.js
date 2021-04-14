77893a5bb76ab07f918f9436d6f3eb6eb3f55f6e
Hossein Margani <margani.h@gmail.com>
2021-04-14 23:09:45 +0100

    Upgrade ssg version

--- build.js
+++ build.js
@@ -1,3 +1,29 @@
 const generator = require("artan-ssg");
+const path = require("path");
+const fs = require("fs");
 
-generator.generate("src", "dist");
+const promises = [];
+
+generator.generate(
+  "src",
+  "dist",
+  {
+    sampleFilter: function (data) {
+      return "nothing";
+    },
+  },
+  {
+    sample: function (data) {
+      return "nothing";
+    },
+  },
+  "debug"
+);
+
+Promise.all(promises)
+  .then(() => {
+    process.stdout.write("\rDone!\x1b[K\n");
+  })
+  .catch((err) => {
+    console.error("Error processing files, let's clean it up", err);
+  });
